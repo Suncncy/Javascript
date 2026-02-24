60d06f5ec5df342efb4b127e6e35865fb9adede1
Suncncy <3378035936@qq.com>
2026-02-24 19:31:51 +0800

    获取元素位置

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-var  root=document.getElementById("root");//打印出id
-//console.log(root.id);
-root.id="roots";//在element里面更改id
-console.log(root.className);//打印出class
-root.className="boxs box1";//修改元素里面的class标签,可以修改成多个标签
-console.log(root.classList.add("mybox"));//进行添加class
-root.classList.remove("boxs");//进行移除class
-if(root.classList.contains("box1")){//进行判断是否包含
-    console.log("yes");
-}else{
-    console.log("no");
-}
-root.classList.toggle("box2");//如果存在即删除，不存在即添加
-//这个是读取root标签里面的内容：console.log(root.innerHTML);
-//这是修改root标签里面的内容console.log(root.innerHTML="南阳理工学院");
-//将innerHTML修改成innerText也有相同的效果，但是innerHTML可以识别标签，但是innerText识别不了标签，会把标签认成字符串
-var str="<a href='https://www.itbaizhan.com'>百战</a>";
-root.innerHTML=str;//这样出现的是百战的链接
-root.innerText=str;//这样在视图里面只是把上面的链接照搬下来
-
-
-
-
+var box=document.getElementById("box")
+console.log(box.clientWidth);
+console.log(box.clientHeight);//只会包含padding的部分，在console里面显示
+console.log(document.documentElement.clientHeight);//屏幕，视图的高度
+console.log(document.body.clientHeight);//所写的盒子所占的高度
+console.log("-----");
+console.log(box.scrollWidth);//和上边的差不多一摸一样
+console.log(box.scrollHeight);
+console.log(document.documentElement.scrollHeight);
+console.log(document.body.scrollHeight);
+console.log("-----");
+console.log(document.documentElement.scrollTop);//滚动高度
+console.log("-----");
+console.log(box.offsetWidth);//包含元素的本身，还有padding，还有border
+console.log(box.offsetHeight);
+console.log("-----");
+console.log(box.offsetLeft);//元素距离左边的距离，还多一个浏览器自带的8px
+console.log(box.offsetTop);//元素距离上边的距离
 
 
